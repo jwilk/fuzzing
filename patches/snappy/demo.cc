a5f6ccd288ed7203e66dc23afe88fdb0151beb48
Jakub Wilk <jwilk@jwilk.net>
2015-02-02 21:51:37 +0100

    patches/snappy: add.

---
+++ patches/snappy/demo.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <sstream>
+
+#include <snappy.h>
+
+std::string slurp()
+{
+	std::ostringstream ss;
+	ss << std::cin.rdbuf();
+	return ss.str();
+}
+
+int main(int argc, char **argv)
+try
+{
+	std::string input, output;
+	input = slurp();
+	if (argc == 2 && std::string(argv[1]) == std::string("-z"))
+		snappy::Compress(input.data(), input.size(), &output);
+	else
+		snappy::Uncompress(input.data(), input.size(), &output);
+	std::cout << output;
+	return 0;
+}
+catch (std::bad_alloc)
+{
+	return 1;
+}
