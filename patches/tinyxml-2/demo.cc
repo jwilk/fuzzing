df266701e6eb71c81d31efe0bf3f1bba63db361f
Jakub Wilk <jwilk@jwilk.net>
2015-03-09 22:14:46 +0100

    patches/tinyxml-2: add.

---
+++ patches/tinyxml-2/demo.cc
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+#include <tinyxml2.h>
+
+using namespace tinyxml2;
+
+int main(int argc, char ** argv)
+{
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+		return 1;
+	}
+	XMLDocument* doc = new XMLDocument();
+	if (doc->LoadFile(argv[1]) != XML_NO_ERROR) {
+		doc->PrintError();
+		return 1;
+	}
+	XMLPrinter printer;
+	doc->Print(&printer);
+	puts(printer.CStr());
+	return 0;
+}
