aa5f39e9a50017ea38a1735f46fbbfc4e8eb594f
Jakub Wilk <jwilk@jwilk.net>
2015-03-09 21:14:58 +0100

    patches/tinyxml: add.

---
+++ patches/tinyxml/demo.cc
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define TIXML_USE_STL 1
+#include <tinyxml.h>
+
+int main(int argc, char ** argv)
+{
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+		return 1;
+	}
+	TiXmlDocument doc(argv[1]);
+	if (!doc.LoadFile()) {
+		fprintf(stderr, "invalid XML\n");
+		return 1;
+	}
+	TiXmlPrinter printer;
+	doc.Accept(&printer);
+	puts(printer.CStr());
+	return 0;
+}
