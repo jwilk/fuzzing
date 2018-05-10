8d2f732f2e648f662a59f12d5f08d66e85a72dc3
Jakub Wilk <jwilk@jwilk.net>
2018-05-10 17:15:04 +0200

    pkgs/snappy/demo: expand tabs.

--- pkgs/snappy/demo.cc
+++ pkgs/snappy/demo.cc
@@ -5,24 +5,26 @@
 
 std::string slurp()
 {
-	std::ostringstream ss;
-	ss << std::cin.rdbuf();
-	return ss.str();
+    std::ostringstream ss;
+    ss << std::cin.rdbuf();
+    return ss.str();
 }
 
 int main(int argc, char **argv)
 try
 {
-	std::string input, output;
-	input = slurp();
-	if (argc == 2 && std::string(argv[1]) == std::string("-z"))
-		snappy::Compress(input.data(), input.size(), &output);
-	else
-		snappy::Uncompress(input.data(), input.size(), &output);
-	std::cout << output;
-	return 0;
+    std::string input, output;
+    input = slurp();
+    if (argc == 2 && std::string(argv[1]) == std::string("-z"))
+        snappy::Compress(input.data(), input.size(), &output);
+    else
+        snappy::Uncompress(input.data(), input.size(), &output);
+    std::cout << output;
+    return 0;
 }
 catch (std::bad_alloc)
 {
-	return 1;
+    return 1;
 }
+
+/* vim:set ts=4 sts=4 sw=4 et:*/
