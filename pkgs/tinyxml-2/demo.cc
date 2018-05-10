15c8401008c49a7f8f9abb53adaffa9e9b637eda
Jakub Wilk <jwilk@jwilk.net>
2018-05-10 17:15:20 +0200

    pkgs/tinyxml-2/demo: expand tabs.

--- pkgs/tinyxml-2/demo.cc
+++ pkgs/tinyxml-2/demo.cc
@@ -6,17 +6,19 @@ using namespace tinyxml2;
 
 int main(int argc, char ** argv)
 {
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-		return 1;
-	}
-	XMLDocument* doc = new XMLDocument();
-	if (doc->LoadFile(argv[1]) != XML_NO_ERROR) {
-		doc->PrintError();
-		return 1;
-	}
-	XMLPrinter printer;
-	doc->Print(&printer);
-	puts(printer.CStr());
-	return 0;
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        return 1;
+    }
+    XMLDocument* doc = new XMLDocument();
+    if (doc->LoadFile(argv[1]) != XML_NO_ERROR) {
+        doc->PrintError();
+        return 1;
+    }
+    XMLPrinter printer;
+    doc->Print(&printer);
+    puts(printer.CStr());
+    return 0;
 }
+
+/* vim:set ts=4 sts=4 sw=4 et:*/
