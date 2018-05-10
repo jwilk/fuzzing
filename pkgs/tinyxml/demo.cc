b448863cff39bbf144654b5d79697f886eb3d02e
Jakub Wilk <jwilk@jwilk.net>
2018-05-10 17:12:36 +0200

    pkgs/tinyxml/demo: expand tabs.

--- pkgs/tinyxml/demo.cc
+++ pkgs/tinyxml/demo.cc
@@ -5,17 +5,19 @@
 
 int main(int argc, char ** argv)
 {
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
-		return 1;
-	}
-	TiXmlDocument doc(argv[1]);
-	if (!doc.LoadFile()) {
-		fprintf(stderr, "invalid XML\n");
-		return 1;
-	}
-	TiXmlPrinter printer;
-	doc.Accept(&printer);
-	puts(printer.CStr());
-	return 0;
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+        return 1;
+    }
+    TiXmlDocument doc(argv[1]);
+    if (!doc.LoadFile()) {
+        fprintf(stderr, "invalid XML\n");
+        return 1;
+    }
+    TiXmlPrinter printer;
+    doc.Accept(&printer);
+    puts(printer.CStr());
+    return 0;
 }
+
+/* vim:set ts=4 sts=4 sw=4 et:*/
